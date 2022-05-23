eaa0ef35865c068d54c35901bb88e141bb71a476
Daniel Brownlow <hello@danbrownlow.uk>
2022-05-23 21:25:08 +0100

    Finish up basics

--- script.js
+++ script.js
@@ -1,47 +1,122 @@
-class Cell
-{
-  static width = 10;
-  static height = 10;
-
-  constructor(context, posX, posY) {
+class Cell {
+  constructor(context, posX, posY, width = 10, height = 10) {
     this.context = context;
+    this.state = Math.random() > 0.5;
     this.posX = posX;
     this.posY = posY;
-    this.alive = Math.random() > 0.5;
+    this.width = width;
+    this.height = height;
+    this.color = '#FF0000';
   }
 
-  draw() 
-  {
-    this.context.fillStyle = this.alive ? '#FF0000' : '#303030';
-    this.context.fillRect(this.posX * Cell.width, this.posY * Cell.height, Cell.width, Cell.height);
+  draw() {
+    this.context.fillStyle = this.state ? this.color : '#303030';
+    this.context.fillRect(this.posX * this.width, this.posY * this.height, this.width, this.height);
   }
 }
-  
-class Game
-{
-  static rows = 50;
-  static cols = 50;
-   
-  constructor(canvasId) 
-  {
+
+class Game {
+  constructor(canvasId, rows = 40, cols = 75) {
     this.canvas = document.getElementById(canvasId);
     this.context = this.canvas.getContext('2d');
+    this.rows = rows;
+    this.cols = cols;
     this.cells = [];
+    this.isPaused = true;
+
     this.generateCells();
+    this.drawCanvas();
+    this.startSimulation();
   }
 
-  generateCells()
-  {
-    for (let x = 0; x < Game.cols; x++)
-      for (let y = 0; y < Game.rows; y++)
-       this.cells.push(new Cell(this.context, x, y));
+  startSimulation() {
+    window.requestAnimationFrame(() => this.run());
+  }
 
-    // Draw cells for test
+  toggleGameState() {
+    console.log(this.isPaused)
+    this.isPaused = !this.isPaused ? true : false;
+  }
+
+  generateCells() {
+    for (let y = 0; y < this.rows; y++)
+      for (let x = 0; x < this.cols; x++)
+        this.cells.push(new Cell(this.context, x, y));
+  }
+
+  getIndex(x, y) {
+    return x + (y * this.cols);
+  }
+
+  getCellState(x, y) {
+    if (x < 0 || x >= this.cols || y < 0 || y >= this.rows)
+      return false;
+
+    return this.cells[this.getIndex(x, y)].state ? 1 : 0;
+  }
+
+  countNeighbours(x, y) {
+    let neighbours = 0;
+
+    if (this.getCellState(x - 1, y - 1)) neighbours++;
+    if (this.getCellState(x - 1, y)) neighbours++;
+    if (this.getCellState(x - 1, y + 1)) neighbours++;
+
+    if (this.getCellState(x, y - 1)) neighbours++;
+    if (this.getCellState(x, y + 1)) neighbours++;
+
+    if (this.getCellState(x + 1, y - 1)) neighbours++;
+    if (this.getCellState(x + 1, y)) neighbours++;
+    if (this.getCellState(x + 1, y + 1)) neighbours++;
+
+    return neighbours;
+  }
+
+  calculateNextState(x, y) {
+    const neighbours = this.countNeighbours(x, y);
+    let nextState = false;
+    const idx = this.getIndex(x, y);
+    const currentState = this.cells[idx].state;
+
+    if (currentState && (neighbours === 2 || neighbours === 3))
+      nextState = true;
+
+    if (!currentState && neighbours === 3)
+      nextState = true;
+
+    this.cells[idx].nextState = nextState;
+  }
+
+  nextTick() {
+    for (let x = 0; x < this.cols; x++) {
+      for (let y = 0; y < this.rows; y++) {
+        this.calculateNextState(x, y);
+      }
+    }
+
+    // apply state
+    for (let i = 0; i < this.cells.length; i++) {
+      this.cells[i].state = this.cells[i].nextState;
+    }
+  }
+
+  drawCanvas() {
     for (let i = 0; i < this.cells.length; i++)
       this.cells[i].draw();
   }
-}
 
-window.onload = () => {
-  let game = new Game('cells');
+  clear() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  run() {
+    this.clear();
+    this.nextTick();
+    this.drawCanvas();
+
+    setTimeout(() => {
+      if (this.isPaused) return;
+      window.requestAnimationFrame(() => this.run());
+    }, 100);
+  }
 }
